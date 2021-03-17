423e1bae19f13426d7b38e6ded9b153e13a21e0f
vanessakoch <vahnkoch@gmail.com>
2021-03-16 23:57:10 -0300

    upload knexfile

--- knexfile.js
+++ knexfile.js
@@ -24,7 +24,10 @@ module.exports = {
     seeds: {
       directory: path.resolve(__dirname, 'src', 'database', 'seeds')
     },
-    useNullAsDefault: true    
+    useNullAsDefault: true,
+    ssl: {
+      rejectUnauthorized: false
+    }  
   },
 
   test: {
