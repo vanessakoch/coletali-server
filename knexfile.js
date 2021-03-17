61b5dc2e7c70fc44ab461c8ec171838131fdb4fc
vanessakoch <vahnkoch@gmail.com>
2021-03-16 22:53:57 -0300

    Configurando postgress na producao

--- knexfile.js
+++ knexfile.js
@@ -15,6 +15,17 @@ module.exports = {
     useNullAsDefault: true
   },
 
+  production: { 
+    client: 'pg', 
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: path.resolve(__dirname, 'src', 'database', 'migrations')
+    },
+    seeds: {
+      directory: path.resolve(__dirname, 'src', 'database', 'seeds')
+    },
+  },
+
   test: {
     client: 'sqlite3',
     connection: {
