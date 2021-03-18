6b172024f2620e4f3cfc3a8d4e947c87670318c6
vanessakoch <vahnkoch@gmail.com>
2021-03-18 01:02:04 -0300

    allow cross domain

--- src/server.js
+++ src/server.js
@@ -7,6 +7,20 @@ require("dotenv").config();
 
 const app = express();
 
+let allowCrossDomain = function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+  if ('OPTIONS' == req.method) {
+    res.send(200);
+  }
+  else {
+    next();
+  }
+};
+
+app.use(allowCrossDomain)
 app.use(cors());
 
 app.use(express.json());
