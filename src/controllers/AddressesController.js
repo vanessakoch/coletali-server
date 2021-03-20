586c16020f4ccec7648e388d53a41e67944e9c32
vanessakoch <vahnkoch@gmail.com>
2021-03-20 20:44:05 -0300

    change

--- src/controllers/AddressesController.js
+++ src/controllers/AddressesController.js
@@ -42,7 +42,7 @@ class AddressesController {
     const address_id = inserted[0];
     await trx.commit();
 
-    return response.send({ id: address_id})
+    return response.send({ id: 1})
   }
 
   async update(request, response) {
