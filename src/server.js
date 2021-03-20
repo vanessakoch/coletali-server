61af4dbf3edc87887293cd596cc8d5a6ae026fe4
vanessakoch <vahnkoch@gmail.com>
2021-03-20 20:03:18 -0300

    enable cors

--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ require("dotenv").config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({ origin: true }));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
