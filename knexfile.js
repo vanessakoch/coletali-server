7f0419385ed9c721aaf811a7a8078d4acfb5f7eb
vanessakoch <vahnkoch@gmail.com>
2021-03-21 16:35:00 -0300

    update

--- knexfile.js
+++ knexfile.js
@@ -17,13 +17,7 @@ module.exports = {
 
   production: {
     client: 'mysql',
-    connection: {
-      host: process.env.DB_HOST,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      supportBigNumbers: true
-    },
+    connection: process.env.CLEARDB_DATABASE_URL,
     migrations: {
       directory: path.resolve(__dirname, 'src', 'database', 'migrations')
     },
