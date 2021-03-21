eea714078cc6f7fd3fcaa9eef4f926c287a1c35b
vanessakoch <vahnkoch@gmail.com>
2021-03-21 16:41:02 -0300

    update sql

--- src/database/migrations/01_create_address.js
+++ src/database/migrations/01_create_address.js
@@ -1,8 +1,8 @@
 exports.up = async function (knex) {
   return knex.schema.createTable('address', table => {
     table.increments('id').primary();
-    table.decimal('latitude').notNullable();
-    table.decimal('longitude').notNullable();
+    table.float('latitude', 14, 10).notNullable();
+    table.float('longitude', 14, 10).notNullable();
     table.integer('number').notNullable();
     table.string('city').notNullable();
     table.string('uf', 2).notNullable();
