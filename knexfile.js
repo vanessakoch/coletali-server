8bd0089ef2d6d605f6ab7c1711b87ddf7fcc8155
vanessakoch <vahnkoch@gmail.com>
2021-03-21 16:29:26 -0300

    mudando a maneira de conexao com mysql

--- knexfile.js
+++ knexfile.js
@@ -17,7 +17,13 @@ module.exports = {
 
   production: {
     client: 'mysql',
-    connection: process.env.CLEARDB_DATABASE_URL,
+    connection: {
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      supportBigNumbers: true
+    },
     migrations: {
       directory: path.resolve(__dirname, 'src', 'database', 'migrations')
     },
