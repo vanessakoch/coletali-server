7abf25e322632ca3610edf9ccc45973bcd339525
vanessakoch <vahnkoch@gmail.com>
2021-03-21 00:37:28 -0300

    update cors

--- src/server.js
+++ src/server.js
@@ -7,33 +7,8 @@ require("dotenv").config();
 
 const app = express();
 
-conf = {
-  originUndefined: function (req, res, next) {
-    if (!req.headers.origin) {
-      res.json({
-        mess: 'Hi you are visiting the service locally. If this was a CORS the origin header shoud not be undefined'
-      });
-    } else {
-      next();
-    }
-  },
-
-  cors: {
-    origin: function (origin, cb) {
-      let wl = ['http://192.168.2.112:3333', 'https://coletali-server.herokuapp.com/'];
-      
-      if (wl.indexOf(origin) != -1) {
-        cb(null, true);
-      } else {
-        cb(new Error('invalid origin: ' + origin), false);
-      }
-    },
-    optionsSuccessStatus: 200
-  }
-};
-
 app.use(express.json());
-app.use(conf.originUndefined, cors(conf.cors));
+app.use(cors());
 
 app.use(routes);
 app.use('/assets', express.static(path.resolve(__dirname, '..', 'assets')));
