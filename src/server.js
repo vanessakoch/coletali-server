d04676ca1850beaa21fd376f0fa288117fd0494c
vanessakoch <vahnkoch@gmail.com>
2021-03-20 22:28:31 -0300

    config cors manualmente

--- src/server.js
+++ src/server.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const routes = require('./routes');
 const path = require('path');
-const cors = require('cors');
 const { errors } = require('celebrate');
 require("dotenv").config();
 
 const app = express();
 
-app.use(cors());
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(routes);
