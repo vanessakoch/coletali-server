c6096db4eccc319ff3d3724db48d698845437233
vanessakoch <vahnkoch@gmail.com>
2021-03-20 22:33:17 -0300

    usando biblioteca cors

--- src/server.js
+++ src/server.js
@@ -1,21 +1,22 @@
 const express = require('express');
+const cors = require('cors');
 const routes = require('./routes');
 const path = require('path');
 const { errors } = require('celebrate');
 require("dotenv").config();
 
 const app = express();
-
-app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  next();
-});
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+const corsOptions = {
+  origin: 'https://your-app-name.herokuapp.com',
+  optionsSuccessStatus: 200
+}
+
+app.use(cors(corsOptions));
+app.options('*', cors());
+
 app.use(routes);
 app.use('/assets', express.static(path.resolve(__dirname, '..', 'assets')));
 app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
