75c2b202dd92216b2fcdce55d6eb9560cfbc2adf
vanessakoch <vahnkoch@gmail.com>
2021-03-20 23:45:46 -0300

    conf cors

--- src/server.js
+++ src/server.js
@@ -7,9 +7,33 @@ require("dotenv").config();
 
 const app = express();
 
-app.use(cors({ origin: 'https://coletali-server.herokuapp.com/'}));
+conf = {
+  originUndefined: function (req, res, next) {
+    if (!req.headers.origin) {
+      res.json({
+        mess: 'Hi you are visiting the service locally. If this was a CORS the origin header shoud not be undefined'
+      });
+
+    } else {
+      next();
+    }
+  },
+
+  cors: {
+    origin: function (origin, cb) {
+      let wl = ['https://coletali-web.herokuapp.com'];
+      if (wl.indexOf(origin) != -1) {
+        cb(null, true);
+      } else {
+        cb(new Error('invalid origin: ' + origin), false);
+      }
+    },
+    optionsSuccessStatus: 200
+  }
+};
 
 app.use(express.json());
+app.use(conf.originUndefined, cors(conf.cors));
 app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
@@ -18,6 +42,6 @@ app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
 app.use(errors());
 
-app.listen(process.env.PORT || 3333, function(){
+app.listen(process.env.PORT || 3333, function () {
   console.log("Server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
