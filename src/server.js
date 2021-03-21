4fdf6e2bc790821426157df79aec565e190bc12f
vanessakoch <vahnkoch@gmail.com>
2021-03-20 22:45:34 -0300

    removing cors

--- src/server.js
+++ src/server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const cors = require('cors');
 const routes = require('./routes');
 const path = require('path');
 const { errors } = require('celebrate');
@@ -8,14 +7,6 @@ require("dotenv").config();
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-const corsOptions = {
-  origin: 'https://coletali-web.herokuapp.com',
-  optionsSuccessStatus: 200
-}
-
-app.use(cors(corsOptions));
-app.options('*', cors());
 
 app.use(routes);
 app.use('/assets', express.static(path.resolve(__dirname, '..', 'assets')));
